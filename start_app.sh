4ee2bae4f819ddfee4dac33eb60948579148d0b6
hongyu <petrel2015@foxmail.com>
2026-03-01 18:13:03 +0800

    chore: cleanup obsolete components, scripts and temporary workspace files

--- start_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home"
-cd /Users/user/Documents/Spark-Performance-Insight
-nohup java -Dinsight.event-log-path=workspace/eventlog -jar workspace/app.jar --spring.config.additional-location=optional:file:workspace/config/application-test.yml > workspace/app.log 2>&1 &
-echo $! > workspace/app.pid
